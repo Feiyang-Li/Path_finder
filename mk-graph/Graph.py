da99614f442ac7e637c880a24c872d222619ecfb
Feiyang-li <feiyang.f.li@gmail.com>
2023-02-18 02:46:17 +0000

    graph creation init

---
+++ mk-graph/Graph.py
@@ -0,0 +1,8 @@
+## Must first consider what is a Graph.
+# Graph is created by the connection of nodes.
+# The purpose of graph is to later find a fast algorithm that get me from A to B
+
+class graph:
+    node = [] 
+    edges = {}
+    
